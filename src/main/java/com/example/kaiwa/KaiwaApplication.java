ee81184428d338f8d3c4c48b4c111f586607aed3
Bartek <bartek32d@gmail.com>
2023-10-15 15:24:15 +0200

    Add initial project files

---
+++ src/main/java/com/example/kaiwa/KaiwaApplication.java
@@ -0,0 +1,13 @@
+package com.example.kaiwa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KaiwaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KaiwaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/kaiwa/KaiwaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kaiwa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KaiwaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
