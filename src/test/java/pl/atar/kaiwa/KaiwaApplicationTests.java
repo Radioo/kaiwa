64d1b6c2801e0e8c7a88c400d24af82c91a86bfb
Bartek <bartek32d@gmail.com>
2023-10-15 15:08:04 +0200

    Initial commit

---
+++ src/test/java/pl/atar/kaiwa/KaiwaApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.atar.kaiwa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KaiwaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
