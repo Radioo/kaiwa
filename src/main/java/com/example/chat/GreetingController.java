bef2f783d27b1842610ce31f2babe36a5eee4fee
Bartek <bartek32d@gmail.com>
2023-10-18 19:52:35 +0200

    Clean up

--- src/main/java/com/example/chat/GreetingController.java
+++ src/main/java/com/example/chat/GreetingController.java
@@ -10,7 +10,7 @@ import java.util.List;
 
 @Controller
 public class GreetingController {
-    private List<Message> messageHistory = new ArrayList<>();
+    private final List<Message> messageHistory = new ArrayList<>();
 
 
     @MessageMapping("/hello")
