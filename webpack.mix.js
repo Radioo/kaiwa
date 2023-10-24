5a5384b148c174042714799a33788a4f4f9ce674
Bartek <bartek32d@gmail.com>
2023-10-24 15:34:27 +0200

    Add Laravel Mix

---
+++ webpack.mix.js
@@ -0,0 +1,10 @@
+let mix = require("laravel-mix");
+
+mix.disableSuccessNotifications();
+
+mix.setPublicPath("src/main/resources/static");
+
+mix.sass("src/main/resources/web-assets/main.scss", "css/app.css");
+
+mix.js("src/main/resources/web-assets/main.js", "js/app.js")
+    .vue({version: 3});
