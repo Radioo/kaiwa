4bf8a6032b1d63da9481f720e9d6ab7796665657
Bartek <bartek32d@gmail.com>
2023-10-25 10:00:41 +0200

    Add example vue component

---
+++ src/main/resources/web-assets/components/Chat.vue
@@ -0,0 +1,15 @@
+<template>
+  <h1>test</h1>
+</template>
+
+<script>
+export default {
+  name: "Chat"
+}
+</script>
+
+<style lang="scss">
+h1 {
+  color: green;
+}
+</style>
--- src/main/resources/web-assets/main.js
+++ src/main/resources/web-assets/main.js
@@ -1,1 +1,8 @@
 import {createApp} from "vue";
+import Chat from "./components/Chat.vue"
+
+createApp({
+    components: {
+        Chat
+    }
+}).mount("#app");
