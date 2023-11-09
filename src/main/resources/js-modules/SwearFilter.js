fcf3ee06f830ca3d16225c2b683ab81852c1e7cc
WW36419 <132059764+WW36419@users.noreply.github.com>
2023-11-09 16:18:50 +0100

    Update SwearFilter.js

--- src/main/resources/js-modules/SwearFilter.js
+++ src/main/resources/js-modules/SwearFilter.js
@@ -1,5 +1,16 @@
-const filerWord = (word) => {
-    return word.toUpperCase()
+const swearFilter = (word) => {
+    const swearWords = ["fuck", "ass", "shit", "bitch", "cock", "dick", "cunt", "fag", "pussy", "slut", "retard", "whore", "nigg", "cip", "pizd", "chuj", "jeb", "pierd", "cwel", "pedał", "pedal", "dziwk", "kurw", "mend", "dup", "gówn", "choler"]
+
+    for (let j in swearWords){
+        let swear = swearWords[j]
+        if(word.toLowerCase().includes(swear)){
+            let censor = ""
+            for (let i = 0; i < swear.length; i++)
+                censor += "*"
+            word = word.replace(new RegExp(swear, "ig"), censor)
+        }
+    }
+    return word.toUpperCase();
 }
 
-export default filerWord;
+export default swearFilter;
