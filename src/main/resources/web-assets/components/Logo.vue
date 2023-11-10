0a2b2470c22c58fdb5cb85be6454ae9bea5139aa
Bartek <bartek32d@gmail.com>
2023-11-10 09:01:13 +0100

    Add missing files

---
+++ src/main/resources/web-assets/components/Logo.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="logo"><img src="../Kaiwa.svg" alt="Logo"></div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss">
+    .logo{
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        width: 60%;
+        margin: auto;
+        img{
+            max-width: 20%;
+            height: auto;
+        }
+    }
+
+</style>
