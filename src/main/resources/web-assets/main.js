a97ed0e5db0325c4308faefc7762390c14fd72a4
miskyy <misiulek0w0@gmail.com>
2023-11-16 23:13:11 +0100

    add vue login component

--- src/main/resources/web-assets/main.js
+++ src/main/resources/web-assets/main.js
@@ -1,4 +1,6 @@
 import { createApp } from "vue";
 import Chat from "./components/Chat.vue";
+import Login from "./components/Login.vue";
 
+createApp(Login).mount("#login");
 createApp(Chat).mount("#chat");
