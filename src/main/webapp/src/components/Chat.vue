8b5e4f572446aa5f6d614cb2f02ac7b3c37bd80f
miskyy <misiulek0w0@gmail.com>
2023-11-28 19:51:21 +0100

    implement counter for unread messages

--- src/main/webapp/src/components/Chat.vue
+++ src/main/webapp/src/components/Chat.vue
@@ -1,17 +1,17 @@
 <template>
-  <UserHeader :username="username"/>
-  <Logo/>
+  <UserHeader :username="username" />
+  <Logo />
   <div class="chat-container">
-      <div class="message-container">
-          <Message v-for="msg in messages" :messageBody="msg" />
-      </div>
+    <div class="message-container">
+      <Message v-for="msg in messages" :messageBody="msg" />
+    </div>
   </div>
 
-  <InputBox @messageSent="addMessage" :username="username"/>
+  <InputBox @messageSent="addMessage" :username="username" />
 </template>
 
 <script setup>
-import {onMounted, ref} from 'vue';
+import { onMounted, ref } from 'vue';
 import Logo from './Logo.vue';
 import Message from './Message.vue';
 import InputBox from './InputBox.vue';
@@ -20,10 +20,22 @@ import UserHeader from "./UserHeader.vue";
 const username = ref("");
 const messages = ref([]);
 
+const unreadMessages = ref(0);
+document.title = (unreadMessages.value === 0 ? "" : `(${unreadMessages.value}) `) + "Kaiwa";
+
+let isFocused = true;
+
+window.addEventListener("blur", async () => {
+  isFocused = false;
+});
+
+window.addEventListener("focus", async () => {
+  isFocused = true;
+  unreadMessages.value = 0;
+});
+
 onMounted(() => {
-  const evtSource = new EventSource(
-      "http://localhost:8080/sse"
-  );
+  const evtSource = new EventSource("/sse");
 
   evtSource.onmessage = (event) => {
     const message = JSON.parse(event.data);
@@ -31,42 +43,46 @@ onMounted(() => {
     console.log(message);
 
     messages.value.push(message);
-  }
+
+    if (!isFocused) {
+      unreadMessages.value += 1;
+    }
+  };
 
   fetch("/username")
-      .then(response => response.text())
-      .then(response => username.value = response);
-})
+    .then(response => response.text())
+    .then(response => username.value = response);
+});
 
 function addMessage(messageBody) {
-    fetch("/send", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify(messageBody)
-    });
+  fetch("/send", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(messageBody)
+  });
 }
 </script>
 
 <style lang="scss">
 @use "../scss/abstracts/index" as s;
 
-    .chat-container {
-        width: 60%;
-        height: 60vh;
-        margin: auto;
-        margin-bottom: 20px;
-        border-bottom: s.$base-border-radius solid s.$primary;
-        background-color: s.$chat;
-        overflow-y: scroll;
-        animation: s.$fadeTransition;
-        .message-container{
-            height: 100%;
-            display: flex;
-            flex-direction: column;
-            justify-content: flex-end;
-        }
-    }
+.chat-container {
+  width: 60%;
+  height: 60vh;
+  margin: auto;
+  margin-bottom: 20px;
+  border-bottom: s.$base-border-radius solid s.$primary;
+  background-color: s.$chat;
+  overflow-y: scroll;
+  animation: s.$fadeTransition;
 
+  .message-container {
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-end;
+  }
+}
 </style>
