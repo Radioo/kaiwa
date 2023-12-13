24914da531d9a0b06ca2c05e6c55c1d3284496f2
miskyy <misiulek0w0@gmail.com>
2023-12-13 10:57:41 +0100

    updated register

--- src/main/webapp/src/components/Login.vue
+++ src/main/webapp/src/components/Login.vue
@@ -1,49 +1,34 @@
 <template>
   <Logo />
-  <form class="login-form" method="post">
-    <input type="text" name="username" class="username-input" required placeholder="Username">
-    <input type="password" name="password" class="password-input" required placeholder="Password">
-    <input type="submit" value="Sign In" class="login-button">
-  </form>
+  <div v-if="register">
+    <Signup/>
+    <p>
+      Already have an account? <a href="register" @click.prevent="register = false">Sign in</a>
+    </p>
+  </div>
+  <div v-else>
+    <LoginForm/>
+    <p>
+      Need an account? <a href="register" @click.prevent="register = true">Sign up here</a>
+    </p>
+  </div>
+
 </template>
 
 <script setup>
 import { ref } from 'vue';
 import Logo from './Logo.vue';
+import LoginForm from './LoginForm.vue';
+import Signup from './Signup.vue';
 
-const username = ref("");
-const password = ref("");
-
-function logIn() {
-    console.log({ Username: username.value, Password: password.value });
-}
+const register = ref(false);
 </script>
 
 <style lang="scss">
 @use "../scss/abstracts/index" as *;
 @use '../scss/test';
 
-@mixin inputstuff{
-  font-size: $font-size-base;
-  margin: $base-margin 0 $base-margin;
-  padding: $base-padding;
-  border-radius: $base-border-radius;
-  border-width: $base-border-thickness;
-  background-color: var(--chat);
-  color: var(--font-color);
-}
-
-.login-form {
-    width: 33%;
-    margin: 0 auto;
-    display: grid;
-    grid-template-rows: repeat(3, 5em);
-    row-gap: 10px;
-    animation: $fadeTransition;
-}
-
-.username-input, .password-input, .login-button {
-    @include inputstuff;
-    animation: $fadeTransition;
+p {
+  text-align: center;
 }
 </style>
--- src/main/webapp/src/components/Signup.vue
+++ src/main/webapp/src/components/Signup.vue
@@ -1,59 +1,105 @@
 <template>
-    <Logo />
-    <form class="signup-form" @submit.prevent="signUp">
-      <h2>Create an account</h2>
-      <input type="text" v-model="username" class="username-input" required placeholder="Username">
-      <input type="password" v-model="password" class="password-input" required placeholder="Password">
-      <input type="submit" value="Sign Up" class="signup-button">
-    </form>
-  </template>
-  
-  <script setup>
-  import { ref } from 'vue';
-  import Logo from './Logo.vue';
-  
-  const username = ref("");
-  const password = ref("");
-  
-  function signUp() {
-      if (!username.value.trim()) {
-        alert("No username provided");
-        return;
-      }
-      if (!password.value) {
-        alert("No password provided");
-        return;
-      }
-      console.log({ Username: username.value, Password: password.value });
+  <h2>Create an account</h2>
+  <form class="signup-form" @submit.prevent="signUp">
+    <input type="text" name="name" v-model.trim="username" class="username-input" required placeholder="Username">
+    <input type="password" name="password" v-model="password" class="password-input" required placeholder="Password">
+    <input type="password" v-model="passwordConfirm" class="password-input-confirm" required
+      placeholder="Confirm password">
+    <input type="submit" value="Sign Up" class="signup-button">
+  </form>
+  <p id="error-message">{{ errorMessage }}</p>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+const username = ref("");
+const password = ref("");
+const passwordConfirm = ref("");
+const errorMessage = ref("");
+
+function signUp(e) {
+  if (!username.value) {
+    errorMessage.value = "Username is required.";
+    return;
   }
-  </script>
-  
-  <style lang="scss">
-  @use "../scss/abstracts/index" as *;
-  @use '../scss/test';
-  
-  @mixin inputstuff{
-    font-size: $font-size-base;
-    margin: $base-margin 0 $base-margin;
-    padding: $base-padding;
-    border-radius: $base-border-radius;
-    border-width: $base-border-thickness;
-    background-color: var(--chat);
-    color: var(--font-color);
+  if (!password.value) {
+    errorMessage.value = "Password is required.";
+    return;
   }
-  
-  .signup-form {
-      width: 33%;
-      margin: 0 auto;
-      display: grid;
-      grid-template-rows: repeat(3, 5em);
-      row-gap: 10px;
-      animation: $fadeTransition;
+  if (password.value !== passwordConfirm.value) {
+    errorMessage.value = "Password input fields do not match.";
+    return;
   }
-  
-  .username-input, .password-input, .signup-button {
-      @include inputstuff;
-      animation: $fadeTransition;
+
+  const registerInfo = {
+    name: username.value,
+    password: password.value,
   }
-  </style>
-  
+
+  fetch("/register", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(registerInfo),
+  }).then((response) => {
+    if (response.status === 400) {
+      errorMessage.value = `Username ${username.value} is already taken. Please choose a different username.`;
+      return;
+    }
+
+    // automatically log in to the new account
+    const loginInfo = new URLSearchParams({ username: username.value, password: password.value });
+    fetch("/", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+      body: loginInfo,
+    }).then((response) => {
+      window.location.replace("/chat");
+    })
+  }).catch((error) => {
+      errorMessage.value = "Something went wrong. Please try again later.";
+      console.error(error);
+    });
+}
+</script>
+
+<style lang="scss">
+@use "../scss/abstracts/index" as *;
+@use '../scss/test';
+
+@mixin inputstuff {
+  font-size: $font-size-base;
+  margin: $base-margin 0 $base-margin;
+  padding: $base-padding;
+  border-radius: $base-border-radius;
+  border-width: $base-border-thickness;
+  background-color: var(--chat);
+  color: var(--font-color);
+}
+
+h2 {
+  text-align: center;
+}
+
+.signup-form {
+  width: 33%;
+  margin: 0 auto;
+  display: grid;
+  grid-template-rows: repeat(4, 5em);
+  row-gap: 10px;
+  animation: $fadeTransition;
+
+  input {
+    @include inputstuff;
+    animation: $fadeTransition;
+  }
+}
+
+#error-message {
+  color: red;
+}
+</style>
