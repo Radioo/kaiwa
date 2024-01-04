5bfef5441afc9c862e4bcd9b4aefd506c8e941ef
miskyy <misiulek0w0@gmail.com>
2024-01-05 00:06:38 +0100

    Add password verification

--- src/main/webapp/src/components/Signup.vue
+++ src/main/webapp/src/components/Signup.vue
@@ -2,7 +2,17 @@
   <h2>Create an account</h2>
   <form class="signup-form" @submit.prevent="signUp">
     <input type="text" name="name" v-model.trim="username" class="username-input" placeholder="Username">
-    <input type="password" name="password" v-model="password" class="password-input" placeholder="Password">
+    <input type="password" name="password" v-model="password" class="password-input" @input="updatePasswordRequirements"
+    placeholder="Password">
+    <div class="password-requirements">
+      <p>Password must include:</p>
+      <ul>
+        <li :class="{ fulfilled: passwordLongEnough }">At least 8 characters</li>
+        <li :class="{ fulfilled: passwordHasCapitalLetter }">One capital letter</li>
+        <li :class="{ fulfilled: passwordHasDigit }">One digit</li>
+        <li :class="{ fulfilled: passwordHasSpecialChar }">One special character (!@$%& etc.)</li>
+      </ul>
+    </div>
     <input type="password" v-model="passwordConfirm" class="password-input-confirm"
       placeholder="Confirm password">
     <input type="submit" value="Sign Up" class="signup-button">
@@ -18,6 +28,20 @@ const password = ref("");
 const passwordConfirm = ref("");
 const errorMessage = ref("");
 
+const passwordLongEnough = ref(false);
+const passwordHasCapitalLetter = ref(false);
+const passwordHasDigit = ref(false);
+const passwordHasSpecialChar = ref(false);
+
+function updatePasswordRequirements(e) {
+  const password = e.target.value;
+
+  passwordLongEnough.value = (password.length >= 8);
+  passwordHasCapitalLetter.value = /[A-Z]/.test(password);
+  passwordHasDigit.value = /\d/.test(password);
+  passwordHasSpecialChar.value = /[^a-zA-Z\d]/.test(password);
+}
+
 function signUp(e) {
   if (!username.value) {
     errorMessage.value = "Username is required.";
@@ -29,6 +53,13 @@ function signUp(e) {
   }
   if (password.value !== passwordConfirm.value) {
     errorMessage.value = "Password input fields do not match.";
+    return;
+  }
+  if (!(passwordLongEnough.value
+    && passwordHasCapitalLetter.value
+    && passwordHasDigit.value
+    && passwordHasSpecialChar.value)) {
+    errorMessage.value = "Password does not meet the requirements; please change the password.";
     return;
   }
 
@@ -89,12 +120,27 @@ h2 {
   width: 33%;
   margin: 0 auto;
   display: grid;
-  grid-template-rows: repeat(4, 5em);
+  grid-template-rows: repeat(5, 5em);
   row-gap: 10px;
   justify-content: space-around;
   grid-template-columns: repeat(1, 100%);
   input {
     @include inputstuff;
+  }
+  
+  .password-requirements {
+    p {
+      text-align: left;
+      margin: 0;
+    }
+    ul {
+      margin: 0.2em;
+    }
+    font-size: 0.7em;
+
+    .fulfilled {
+      color: green;
+    }
   }
 }
 
